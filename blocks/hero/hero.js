8730cf842129673ee465ce09ffe9dc4bc46ab5dd
William Jones <billy@freshjones.com>
2023-06-22 09:11:29 -0400

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -131,6 +131,6 @@ export default async function decorate(block) {
   });
 
   const bug = document.createElement('div');
-    bug.innerHTML = '<div style="background:magenta; color: white; font-size:18px; line-height:18px; display:inline;">Hero Bug</div'
+    bug.innerHTML = '<div style="background:magenta; color: white; font-size:18px; line-height:18px; display:inline;">HERO BUG</div'
     block.append(bug);
 }
