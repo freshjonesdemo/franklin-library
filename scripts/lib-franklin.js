2f8883ee898dbd583cb5740f48649d65bd7dd0fa
William Jones <billy@freshjones.com>
2023-08-08 11:50:55 -0400

    test

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -686,15 +686,15 @@ export function librarySetup() {
   window.hlx.libraryBasePath = '';
   window.hlx.lighthouse = new URLSearchParams(window.location.search).get('lighthouse') === 'on';
 
-  // const scriptEl = document.querySelector('script[src$="/scripts/scripts.js"]');
-  // if (scriptEl) {
-  //   try {
-  //     [window.hlx.codeBasePath] = new URL(scriptEl.src).pathname.split('/scripts/scripts.js');
-  //   } catch (error) {
-  //     // eslint-disable-next-line no-console
-  //     console.log(error);
-  //   }
-  // }
+  const scriptEl = document.querySelector('script[src$="/scripts/scripts.js"]');
+  if (scriptEl) {
+    try {
+      [window.hlx.codeBasePath] = new URL(scriptEl.src).pathname.split('/scripts/scripts.js');
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  }
 
   window.hlx.libraryBasePath = '/lib'
   window.hlx.CDNBasePath = 'https://regal-faloodeh-256aa9.netlify.app'
